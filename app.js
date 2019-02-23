13491fed618c57c1be58164406ac64e365894b93
Bartosz Pranczke <bartosz.pranczke@netguru.pl>
2019-02-23 21:04:25 +0100

    Use mdns to obtain gateway ip

--- app.js
+++ app.js
@@ -1,114 +1,29 @@
-const CLI = require('clui'),
-  clear = CLI.Clear,
-  clc = require('cli-color'),
-  Line = CLI.Line,
-  Progress = CLI.Progress,
-  coap = require('node-coap-client').CoapClient;
- 
-require('dotenv').load();
+var mdns = require('mdns-js');
 
-var percent = 0
-
-// var mdns = require('mdns-server')({
-//   reuseAddr: true, // in case other mdns service is running
-//   loopback: true,  // receive our own mdns messages
-//   noInit: true     // do not initialize on creation
-// })
-const gatewayUrl = 'coaps://192.168.0.17:5684'
-coap.setSecurityParams(gatewayUrl,
-  {
-    psk: {
-      'IDENTITY': process.env.PSK
-    }
-  }
-);
-coap
-  .request(
-     gatewayUrl + '/15001/65537',
-    'get'
-  )
-  .then(response => {
-    percent = JSON.parse(response.payload.toString())['3311'][0]['5851'] / 254;
+function discoverGateway() {
+  var browser = mdns.createBrowser();
+  return new Promise((resolve, reject) => {
+    const mdnsTimeout = 3000;
+    browser.on('update', function (data) {
+      if (data.hasOwnProperty('fullname')) {
+        browser.stop();
+        console.log(data.addresses[0]);
+        const gatewayUrl = data.addresses[0] + ':5684'
+        resolve({
+          gatewayUrl
+        });
+      }
+    });
+    browser.on('ready', function () {
+      browser.discover();
+    });
   })
-  .catch(err => { console.log('ERROR'); }
-  );
-
-function sendPut(payload) {
-  coap
-    .request(
-       gatewayUrl + '/15001/65537',
-      'put',
-      Buffer.from(JSON.stringify(payload))
-    )
-    .catch(err => { console.log('ERROR'); }
-    );
-}
-function drawProgress() {
-  // clear()
-
-  var blankLine = new Line().fill().output();
-
-  var headers = new Line()
-    .padding(2)
-    .column('Item', 20, [clc.cyan])
-    .column('Progress', 40, [clc.cyan])
-    .fill()
-    .output();
-
-  blankLine.output();
-
-  var thisPercentBar = new Progress(20);
-  var percentLine = new Line()
-    .padding(2)
-    .column('Item %', 20, [clc.yellow])
-    .column(thisPercentBar.update(percent), 40)
-    .fill()
-    .output()
-
-  blankLine.output();
 }
 
-var statusTimer = setInterval(drawProgress, 1000);
+async function init() {
+  const urlR = discoverGateway().then(r => r);
+  const url = await urlR;
+  require('./tradfri').start("coaps://" + url.gatewayUrl);
+}
 
-const readline = require('readline');
-readline.emitKeypressEvents(process.stdin);
-process.stdin.setRawMode(true);
-process.stdin.on('keypress', (str, key) => {
-  if (key.ctrl && key.name === 'c') {
-    process.exit();
-  }
-  switch (key.name) {
-    case 'up':
-      percent += 0.05;
-      var payload = {
-        '3311': [
-          {
-            '5850': 1,
-            '5851': parseInt(percent * 254)
-          }
-        ]
-      };
-      sendPut(payload);
-      break;
-    case 'down':
-      percent -= 0.05;
-      var payload = {
-        '3311': [
-          {
-            '5850': 1,
-            '5851': parseInt(percent * 254)
-          }
-        ]
-      };
-      sendPut(payload);
-      break;
-    case 'left':
-      console.log(percent);
-      break;
-    case 'right':
-      console.log('right');
-      break;
-  }
-});
-
-console.log('...');
+init();
---
+++ tradfri.js
@@ -0,0 +1,113 @@
+function start(gatewayUrl) {
+  const CLI = require('clui'),
+    clear = CLI.Clear,
+    clc = require('cli-color'),
+    Line = CLI.Line,
+    Progress = CLI.Progress,
+    coap = require('node-coap-client').CoapClient;
+  
+  require('dotenv').load();
+
+  var percent = 0
+
+  // const gatewayUrl = 'coaps://192.168.0.17:5684'
+  coap.setSecurityParams(gatewayUrl,
+    {
+      psk: {
+        'IDENTITY': process.env.PSK
+      }
+    }
+  );
+  coap
+    .request(
+      gatewayUrl + '/15001/65537',
+      'get'
+    )
+    .then(response => {
+      percent = JSON.parse(response.payload.toString())['3311'][0]['5851'] / 254;
+    })
+    .catch(err => { console.log('ERROR'); }
+    );
+
+  function sendPut(payload) {
+    coap
+      .request(
+        gatewayUrl + '/15001/65537',
+        'put',
+        Buffer.from(JSON.stringify(payload))
+      )
+      .catch(err => { console.log('ERROR'); }
+      );
+  }
+  function drawProgress() {
+    // clear()
+
+    var blankLine = new Line().fill().output();
+
+    var headers = new Line()
+      .padding(2)
+      .column('Item', 20, [clc.cyan])
+      .column('Progress', 40, [clc.cyan])
+      .fill()
+      .output();
+
+    blankLine.output();
+
+    var thisPercentBar = new Progress(20);
+    var percentLine = new Line()
+      .padding(2)
+      .column('Item %', 20, [clc.yellow])
+      .column(thisPercentBar.update(percent), 40)
+      .fill()
+      .output()
+
+    blankLine.output();
+  }
+
+  var statusTimer = setInterval(drawProgress, 1000);
+
+  const readline = require('readline');
+  readline.emitKeypressEvents(process.stdin);
+  process.stdin.setRawMode(true);
+  process.stdin.on('keypress', (str, key) => {
+    if (key.ctrl && key.name === 'c') {
+      process.exit();
+    }
+    switch (key.name) {
+      case 'up':
+        percent += 0.05;
+        var payload = {
+          '3311': [
+            {
+              '5850': 1,
+              '5851': parseInt(percent * 254)
+            }
+          ]
+        };
+        sendPut(payload);
+        break;
+      case 'down':
+        percent -= 0.05;
+        var payload = {
+          '3311': [
+            {
+              '5850': 1,
+              '5851': parseInt(percent * 254)
+            }
+          ]
+        };
+        sendPut(payload);
+        break;
+      case 'left':
+        console.log(percent);
+        break;
+      case 'right':
+        console.log('right');
+        break;
+    }
+  });
+
+  console.log('...');
+}
+
+module,exports.start = start;
